7ad1485a6fc3d9c8f076cd9b6fc2343b7fb5990b
ZifangZhao <zifangzhao@gmail.com>
2025-01-16 14:15:56 -0500

    Filter update: THeta changed to 4-12 Hz

---
+++ MCU/Inc/fdacoefs_HTF_T_4_12Hz@1000_ord1_SOS.h
@@ -0,0 +1,39 @@
+#include "tmwtypes.h"
+#define MWSPT_NSEC_HTF_T 5
+const int NL_HTF_T [MWSPT_NSEC_HTF_T][3] = { 1,3,1,3,1 };
+const real32_T NUM_HTF_T [MWSPT_NSEC_HTF_T][3] = {
+{ 
+-0.21779, 0, 0,
+ },
+{ 
+1, -1.0355, 0,
+ },
+{ 
+1, 0, 0,
+ },
+{ 
+1, -1.2568, 4.1763,
+ },
+{ 
+1, 0, 0,
+ }
+};
+const int DL_HTF_T [MWSPT_NSEC_HTF_T][3] = { 1,3,1,3,1 };
+const real32_T DEN_HTF_T [MWSPT_NSEC_HTF_T][3] = {
+{ 
+1, 0, 0,
+ },
+{ 
+1, -0.87877, 0,
+ },
+{ 
+1, 0, 0,
+ },
+{ 
+1, -1.1546, 0.56433,
+ },
+{ 
+1, 0, 0,
+ }
+};
+
